e086ce7b8b33e8f49a26e804948b54deb08b11d0
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-09-13 18:43:36 +0700

    test app to prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,16 @@
 //webpack.mix.js
 const mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "public/js")
+mix.webpackConfig({}).browserSync({
+    proxy: 'localhost:8000',
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ]
+})
+    .js("resources/js/app.js", "public/js")
+    .js("resources/js/theme.js", "public/js/theme.js")
     .sourceMaps()
-    .webpackConfig({
-            devServer: {
-                port: '8079'
-            }
-        }
-    )
-    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]).version();
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
