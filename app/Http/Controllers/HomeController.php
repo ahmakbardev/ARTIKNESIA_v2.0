6b80ab589bf60f128224d0efb101b3ec9c2409cb
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2024-11-14 18:35:50 +0700

    Update detail art , product recommendation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
     public function index(): View
     {
         $art_categories = JenisKarya::query()->limit(4)->get();
-        $art_recommendations = Karya::query()->with(['seniman'])->limit(4)->get();
+        $art_recommendations = Karya::query()->where('status', 'accepted')->with(['seniman'])->limit(4)->get();
         $arts = Karya::query()->with('seniman')->limit(10)->get();
         $articles = Article::query()->with('author:id,name')->orderBy('created_at', 'desc')->limit(10)->get();
         return view('pages.index', compact('art_categories', 'arts', 'art_recommendations', 'articles'));
