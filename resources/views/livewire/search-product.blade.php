62e6727d555eb7c4d0a8126c3c271aab7b0817b0
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2024-11-27 16:36:01 +0700

    Update search image

--- resources/views/livewire/search-product.blade.php
+++ resources/views/livewire/search-product.blade.php
@@ -9,17 +9,16 @@
     />
     @if($query && $results->isNotEmpty())
         <div class="absolute left-0 mt-2 bg-white w-full drop-shadow-lg p-3 rounded-xl z-10">
-            @foreach($results as $article)
+            @foreach($results as $item)
                 <div class="grid grid-cols-8 gap-3 items-center mb-3">
                     <div class="col-span-1">
-                        <img src="{{ $article->thumbnail ?? 'https://placehold.co/600x400' }}"
-                             alt="{{ $article->title }}"
+                        <img src="{{ Str::startsWith($item->images[0], 'http') ? $item->images[0] : env('MEDIA_URL').'/'.$item->images[0] }}"
+                             alt="{{ $item->title }}"
                              class="w-full object-cover rounded-xl"/>
                     </div>
                     <div class="col-span-7">
-                        <h1 class="text-lg font-bold">{{ $article->name }}</h1>
-{{--                        <p class="text-sm">{{ Str::limit($article->content, 100) }}</p>--}}
-                        <a href="{{ route('art', $article->id) }}" class="text-sm text-primary">Lihat lebih detail</a>
+                        <h1 class="text-lg font-bold">{{ $item->name }}</h1>
+                        <a href="{{ route('art', $item->id) }}" class="text-sm text-primary">Lihat lebih detail</a>
                     </div>
                 </div>
             @endforeach
