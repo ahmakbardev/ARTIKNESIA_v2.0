a07ccedd06bd5e12aeb9f1382abdf0a169de0df9
Bariq Firjatullah <bariqfirjatullah1803@gmail.com>
2025-01-26 19:25:10 +0700

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'owngray' : '#EDEDED',
-        'navInfoGray' : '#737373',
-        'primary' : '#DFBE65',
-        'primary-darker' : '#DAA515',
-      },
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      screens: {
-        '2xs': '321px',
-        'xs': '426px',
-      },
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                sm: '600px',
+                md: '728px',
+                lg: '984px',
+                xl: '1240px',
+                '2xl': '1496px',
+            },
+        },
+        extend: {
+            colors: {
+                'owngray': '#EDEDED',
+                'navInfoGray': '#737373',
+                'primary': '#DFBE65',
+                'primary-darker': '#DAA515',
+            },
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            screens: {
+                '2xs': '321px',
+                xs: '426px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
