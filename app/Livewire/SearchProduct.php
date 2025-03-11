cc1a694f0403c990900752b79fbe5bbdd2f7ed09
Hafid <hafid@gmail.com>
2025-03-11 23:13:33 +0700

    feat: add live search filtering for articles and artworks

--- app/Livewire/SearchProduct.php
+++ app/Livewire/SearchProduct.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
 use App\Models\Karya;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class SearchProduct extends Component
@@ -12,9 +14,22 @@ class SearchProduct extends Component
     public string $query = '';
     public array|Collection $results = [];
 
-     // Search By
+    public $selectedItem;
+    // Search By
     public bool $isOpen = false;
 
+    // Event for Dropdown
+    #[On('hideDropdown')]
+    public function clearQuery()
+    {
+        $this->query = '';
+    }
+    public function selectItem($item)
+    {
+
+        $this->selectedItem = $item;
+        $this->isOpen = !$this->isOpen;
+    }
     public function toggleSearchBy()
     {
         $this->isOpen = !$this->isOpen;
@@ -22,10 +37,18 @@ class SearchProduct extends Component
 
     public function updatedQuery()
     {
-        $this->results = Karya::query()->where('name', 'like', '%' . $this->query . '%')
-            ->where('status', 'accepted')
-            ->take(5)
-            ->get();
+        if ($this->selectedItem === 'By Karya' || !$this->selectedItem) {
+            $this->results = Karya::query()->where('name', 'like', '%' . $this->query . '%')
+                ->where('status', 'accepted')
+                ->take(5)
+                ->get();
+        } else {
+            $this->results = Article::query()->where('title', 'like', '%' . $this->query . '%')
+                ->where('status', 'publish')
+                ->take(5)
+                ->get();
+        }
+
     }
 
     public function render(): View
--- resources/views/livewire/search-product.blade.php
+++ resources/views/livewire/search-product.blade.php
@@ -1,5 +1,5 @@
 @php
-    $searchBy = ['By Tags', 'By Title'];
+    $searchBy = ['By Karya', 'By Article'];
 @endphp
 
 <div class="relative">
@@ -8,20 +8,27 @@
             <img src="{{ asset('images/icons/search.svg') }}" alt="">
         </label>
         <input type="text" name="search"
-               class="flex-1 rounded-s-md border bg-gray-50 px-10 py-2 text-sm focus:outline-primary/50"
-               placeholder="Cari di Artiknesia"
-               wire:model.live="query" />
+            class="flex-1 rounded-s-md border bg-gray-50 px-10 py-2 text-sm focus:outline-primary/50"
+            placeholder="Cari di Artiknesia" wire:model.live="query" />
         <div class="relative">
-            <button class="rounded-e-md border bg-gray-50 pl-4 py-2 pr-10 text-sm text-neutral-400" wire:click="toggleSearchBy">
-                Search By
+            <button class="rounded-e-md border bg-gray-50 pl-4 py-2 pr-10 text-sm text-neutral-400"
+                wire:click="toggleSearchBy">
+                @if (!$selectedItem)
+                    Search By
+                @else
+                    {{ $selectedItem }}
+                @endif
                 <div class="absolute inset-y-0 end-0 flex items-center pe-2">
                     <img src="{{ asset('images/icons/caret-small-down.svg') }}" class="size-5" alt="">
                 </div>
             </button>
-            <div class="{{ $isOpen ? '' : 'hidden' }} absolute left-0 right-0 top-[calc(100%)] rounded border bg-gray-50 text-sm transition-all duration-300">
+            <div
+                class="{{ $isOpen ? '' : 'hidden' }} absolute left-0 right-0 top-[calc(100%)] rounded border bg-gray-50 text-sm transition-all duration-300">
                 <ul class="flex flex-col gap-0.5 p-0.5 text-start">
                     @foreach ($searchBy as $item)
-                        <li class="cursor-pointer rounded px-4 py-2 text-neutral-400 hover:bg-primary hover:text-white">
+                        <li class="cursor-pointer rounded px-4 py-2 text-neutral-400 hover:bg-primary hover:text-white"
+                            wire:click="selectItem(
+                            '{{ $item }}')">
                             {{ $item }}
                         </li>
                     @endforeach
@@ -29,25 +36,54 @@
             </div>
         </div>
     </div>
-    @if($query && $results->isNotEmpty())
-        <div class="absolute left-0 mt-2 bg-white w-full drop-shadow-lg p-3 rounded-xl z-10">
-            @foreach($results as $item)
-                <div class="grid grid-cols-8 gap-3 items-center mb-3">
-                    <div class="col-span-1">
-                        <img src="https://artiknesia.com/seniman/storage/{{ $item->images[0]}}"
-                             alt="{{ $item->title }}"
-                             class="w-full object-cover rounded-xl"/>
+    @if ($query && $results->isNotEmpty())
+        @if ($selectedItem === 'By Karya' || !$selectedItem)
+            <div wire:ignore x-ref="dropdown"
+                class="absolute left-0 mt-2 bg-white w-full drop-shadow-lg p-3 rounded-xl z-10">
+                @foreach ($results as $item)
+                    <div class="grid grid-cols-8 gap-3 items-center mb-3">
+                        <div class="col-span-1">
+                            <img src="https://artiknesia.com/seniman/storage/{{ $item->images[0] }}"
+                                alt="{{ $item->title }}" class="w-full object-cover rounded-xl" />
+                        </div>
+                        <div class="col-span-7">
+                            <h1 class="text-lg font-bold">{{ $item->name }}</h1>
+                            <a href="{{ route('art', $item->id) }}" class="text-sm text-primary">Lihat lebih detail</a>
+                        </div>
                     </div>
-                    <div class="col-span-7">
-                        <h1 class="text-lg font-bold">{{ $item->name }}</h1>
-                        <a href="{{ route('art', $item->id) }}" class="text-sm text-primary">Lihat lebih detail</a>
+                @endforeach
+            </div>
+        @else
+            <div class="absolute left-0 mt-2 bg-white w-full drop-shadow-lg p-3 rounded-xl z-10">
+                @foreach ($results as $item)
+                    <div wire:ignore x-ref="dropdown"  class="grid grid-cols-8 gap-3 items-center mb-3">
+                        <div class="col-span-1">
+                            <img src="{{ \Illuminate\Support\Facades\Storage::disk('public')->url($item->image) }}"
+                                alt="{{ $item->title }}" class="w-full object-cover rounded-xl" />
+                        </div>
+                        <div class="col-span-7">
+                            <h1 class="text-lg font-bold">{{ $item->title }}</h1>
+                            <a href="{{ route('art', $item->id) }}" class="text-sm text-primary">Lihat lebih detail</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
+        @endif
     @elseif($query)
         <div class="absolute left-0 mt-2 bg-white w-full drop-shadow-lg p-3 rounded-xl z-10">
             <p class="text-sm text-gray-500">No results found.</p>
         </div>
     @endif
 </div>
+
+<script>
+    document.addEventListener('click', function(event) {
+        let dropdown = document.querySelector('[x-ref = "dropdown"]');
+        let input = document.querySelector('[wire\\:model\\.live="query"]');
+
+        if (dropdown && !dropdown.contains(event.target) && !input.contains(event.target)) {
+            Livewire.dispatch('hideDropdown')
+
+        }
+    })
+</script>
