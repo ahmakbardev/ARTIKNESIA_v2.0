40caccb851a5521836d379f25b130dd073b51338
akhnaf-artiknesia <akhnaf.artiknesia@gmail.com>
2025-04-23 15:17:51 +0700

    pameran 2 banner based on created_date terbaru

--- app/Http/Controllers/ExhibitionController.php
+++ app/Http/Controllers/ExhibitionController.php
@@ -10,18 +10,27 @@ class ExhibitionController extends Controller
 {
     public function index(): View
     {
-        $exhibition = Exhibition::query()
+        $exhibitions = Exhibition::query()
             ->where('is_primary', 1)
             ->orderBy('updated_at', 'desc')
             ->first();
 
-        if (!$exhibition) {
-            $exhibition = Exhibition::query()
-                ->inRandomOrder()
-                ->first();
+        if (!$exhibitions) {
+            $exhibitions = Exhibition::query()
+                // ->whereDate('updated_at', '>=', now())
+                ->orderBy('updated_at', 'desc')
+                ->take(2)
+                ->get();
+            // dd($exhibitions);
+
+            if (!$exhibitions) {
+                $exhibitions = Exhibition::query()
+                    ->inRandomOrder()
+                    ->first();
+            }
         }
 
-        return view('pages.exhibition.index', compact('exhibition'));
+        return view('pages.exhibition.index', compact('exhibitions'));
     }
 
     public function show(Exhibition $exhibition): View
